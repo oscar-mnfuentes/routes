4a11b2aadfcf36d2b15bfb29b0cdda8c1ee75413
Cris Pampin <cristina.rpampin@gmail.com>
2020-05-02 11:37:02 +0200

    routes changes

--- functions/src/repository/impl/routeRepositoryImpl.ts
+++ functions/src/repository/impl/routeRepositoryImpl.ts
@@ -1,14 +1,14 @@
 import { RouteRepository } from "../routeRepository"
 import { firestore } from "../.."
-import { DocumentReference, CollectionReference, DocumentSnapshot } from "@google-cloud/firestore"
+import { DocumentReference, CollectionReference, DocumentSnapshot, QuerySnapshot } from "@google-cloud/firestore"
 import { Route } from "../../interfaces/Route"
 
 export class RouteRepositoryImpl implements RouteRepository {
   getRoute(idRoute: string): Promise<DocumentSnapshot> {
     return firestore.collection("routes").doc(idRoute).get()
   }
-  getRoutes(): Promise<DocumentReference[]> {
-    return firestore.collection("routes").listDocuments()
+  getRoutes(): Promise<QuerySnapshot> {
+    return firestore.collection("routes").get()
   }
   saveNewRoute(route: Route): Promise <DocumentReference> {
     return firestore.collection("routes").add(route)
