f8ea7b9d03c486de8af4fe8254f5f3cc69d377e6
Cris Pampin <cristina.rpampin@gmail.com>
2020-05-02 11:58:32 +0200

    routeService functions

--- functions/src/service/impl/routeServiceImpl.ts
+++ functions/src/service/impl/routeServiceImpl.ts
@@ -1,6 +1,7 @@
 import { RouteService } from "../routeService"
 import { RouteRepositoryImpl } from "../../repository/impl/routeRepositoryImpl"
 import { Route } from "../../interfaces/Route"
+import { Timestamp } from "@google-cloud/firestore"
 
 export class RouteServiceImpl implements RouteService {
   routeRepository: RouteRepositoryImpl
@@ -18,5 +19,30 @@ export class RouteServiceImpl implements RouteService {
         description: routeDoc.data().description,
       }
     })
+  }
+
+  async getRoute(idRoute: string): Promise<Route> {
+    const routeSnapshot = await this.routeRepository.getRoute(idRoute)
+    const route : Route = {
+      id: routeSnapshot.id,
+      name: routeSnapshot.data().name,
+      description: routeSnapshot.data().description
+    }
+    return route
+  }
+
+  async saveNewRoute(route: Route): Promise<string> {
+    const routeSnapshot = await this.routeRepository.saveNewRoute(route)
+    return routeSnapshot.id
+  }
+
+  async updateRoute(route: Route): Promise<Timestamp> {
+    const routeSnapshot = await this.routeRepository.updateRoute(route)
+    return routeSnapshot.writeTime
+  }
+
+  async deleteRoute(idRoute: string): Promise<Timestamp> {
+    const routeSnapshot = await this.routeRepository.deleteRoute(idRoute)
+    return routeSnapshot.writeTime
   }
 }
